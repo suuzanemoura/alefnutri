93ec444ab8a7c6e5db74375b94568048d2525365
suuzanemoura <suuzanemoura@gmail.com>
2024-03-28 21:16:31 -0300

    :sparkles: feat: adicionando a configuração inicial do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1 className="text-id-alef-red-300">Alef Rodrigues | Nutricionista</h1>
+      <p className="text-id-alef-blue-100">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error nesciunt
+        quo voluptates. Neque, ducimus. Iure perferendis labore magnam, illum
+        numquam obcaecati dignissimos! Suscipit fugit soluta explicabo non
+        perferendis commodi ullam?
       </p>
+      <button className="btn btn-id-alef-blue-200">Teste</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
